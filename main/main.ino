3490e977f06bdbe6bfef66dd293ad10680afe9d4
xuziao <xuziaoder@gmail.com>
2023-03-04 20:28:58 +0800

    库都找齐了，开干

--- main/main.ino
+++ main/main.ino
@@ -9,17 +9,17 @@
 MPU9250 mpu;
 
 void setup() {
-  Serial.begin(115200)
+  Serial.begin(115200);
   Wire.begin(); //初始化陀螺仪
   mpu.setup(MPU_ADDRESS);
   mpu.setMagneticDeclination(DECLINATION);
-  pinMode(SWITCH_PIN, INPUT)  //初始化开关
-  while(!digitalRead(SWITCH_PIN)){} //摁下开关后程序运行
+  pinMode(SWITCH_PIN, INPUT);  //初始化开关
+  //while(!digitalRead(SWITCH_PIN)){} //摁下开关后程序运行
   Serial.println("陀螺仪将在5秒内校准\n请将其放置于水平面");
-  mpu.verbose(ture);
+  mpu.verbose(true);
   delay(5000);
   mpu.calibrateAccelGyro();
-  Serial.Println("指南针将在5秒内校准\n请在桌面上画\"8\"字形");
+  Serial.println("指南针将在5秒内校准\n请在桌面上画\"8\"字形");
   delay(5000);
   mpu.calibrateMag();
   mpu.verbose(false);
@@ -27,9 +27,9 @@ void setup() {
 
 void loop() {
   if(mpu.update()){
-    static uint32_t prev_ms millis();
+    static uint32_t prev_ms = millis();
     if(millis() > prev_ms + 25){
-      Serial.print(mpu.getYaw(), 2)
+      Serial.print(mpu.getYaw(), 2);
       prev_ms = millis();
     }
   }
